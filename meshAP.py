58d0f9b759dee738b9c30007ae2229d6edd00816
Manuela CB <m.calvoba@alumnos.urjc.es>
2020-07-21 10:01:55 +0200

    Estructura memoria

--- meshAP.py
+++ meshAP.py
@@ -1,26 +1,27 @@
 #!/usr/bin/python
 
-'This example shows how to create wireless link between two APs'
+"""
+This example shows on how to create wireless link between two APs with mesh
+The wireless mesh network is based on IEEE 802.11s
+"""
 
-from mininet.node import Controller
 from mininet.log import setLogLevel, info
 from mn_wifi.link import wmediumd, mesh
-from mn_wifi.cli import CLI_wifi
+from mn_wifi.cli import CLI
 from mn_wifi.net import Mininet_wifi
 from mn_wifi.wmediumdConnector import interference
 
 
 def topology():
     "Create a network."
-    net = Mininet_wifi(controller=Controller, link=wmediumd,
-                       wmediumd_mode=interference)
+    net = Mininet_wifi(link=wmediumd, wmediumd_mode=interference)
 
     info("*** Creating nodes\n")
     sta1 = net.addStation('sta1', mac='00:00:00:00:00:11', position='1,1,0')
     sta2 = net.addStation('sta2', mac='00:00:00:00:00:12', position='31,11,0')
-    ap1 = net.addAccessPoint('ap1', wlans=2, ssid='ssid1,', position='10,10,0')
-    ap2 = net.addAccessPoint('ap2', wlans=2, ssid='ssid2,', position='30,10,0')
-    c0 = net.addController('c0')
+    ap1 = net.addAccessPoint('ap1', wlans=2, ssid='ssid1', position='10,10,0')
+    ap2 = net.addAccessPoint('ap2', wlans=2, ssid='ssid2', position='30,10,0')
+    # c0 = net.addController('c0')
 
     info("*** Configuring wifi nodes\n")
     net.configureWifiNodes()
@@ -38,7 +39,7 @@ def topology():
     ap2.start([c0])
 
     info("*** Running CLI\n")
-    CLI_wifi(net)
+    CLI(net)
 
     info("*** Stopping network\n")
     net.stop()
