2cd536f91639ea006c7eb2622aa5b3bdb66123f0
manuelacb <64306662+manuelacb@users.noreply.github.com>
2020-08-27 00:55:04 +0200

    Add files via upload

--- topo_tfg.py
+++ topo_tfg.py
@@ -16,9 +16,9 @@ def topology():
 
     info("*** Creating nodes\n")
     sta1 = net.addStation('sta1', mac='00:00:00:00:00:01', IP='10.0.0.1/8', position='10,80,0')
-    sta2 = net.addStation('sta2', wlans=2, mac='00:00:00:00:00:02', IP='10.0.0.2/8', position='20,80,0')
+    sta2 = net.addStation('sta2', mac='00:00:00:00:00:02', IP='10.0.0.2/8', position='20,80,0')
     sta3 = net.addStation('sta3', mac='00:00:00:00:00:03', IP='10.0.0.3/8', position='30,80,0')
-    ap1 = net.addAccessPoint('ap1', wlans=4, ssid='ssid1,,,', position='20,100,0')
+    ap1 = net.addAccessPoint('ap1', wlans=2, ssid='ssid1,,,', position='20,100,0')
 
     sta4 = net.addStation('sta4', wlans=3, mac='00:00:00:00:00:04', IP='10.0.0.4/8', position='50,80,0')
     sta5 = net.addStation('sta5', wlans=2, mac='00:00:00:00:00:05', IP='10.0.0.5/8', position='40,65,0')
@@ -30,7 +30,7 @@ def topology():
     sta9 = net.addStation('sta9', wlans=3, mac='00:00:00:00:00:09', IP='10.0.0.9/8', position='35,40,0')
     sta10 = net.addStation('sta10', mac='00:00:00:00:00:10', IP='10.0.0.10/8', position='30,30,0')
     sta11 = net.addStation('sta11', mac='00:00:00:00:00:11', IP='10.0.0.11/8', position='40,30,0')
-    ap3 = net.addAccessPoint('ap3', wlans=3, ssid='ssid3,,', position='32,50,0')
+    ap3 = net.addAccessPoint('ap3', wlans=2, ssid='ssid3,', position='30,50,0')
 
 
     c0 = net.addController('c0')
@@ -60,9 +60,9 @@ def topology():
     net.addLink(sta9, channel=35, cls=ITSLink)
     net.addLink(sta11, channel=35, cls=ITSLink)
 
-    # net.addLink(ap1, intf='ap1-wlan4', cls=mesh, ssid='mesh-ssid', channel=5)
-    # net.addLink(ap2, intf='ap2-wlan2', cls=mesh, ssid='mesh-ssid', channel=5)
-    # net.addLink(ap3, intf='ap3-wlan3', cls=mesh, ssid='mesh-ssid', channel=5)
+    net.addLink(ap1, intf='ap1-wlan2', cls=mesh, ssid='mesh-ssid', channel=5)
+    net.addLink(ap2, intf='ap2-wlan2', cls=mesh, ssid='mesh-ssid', channel=5)
+    net.addLink(ap3, intf='ap3-wlan2', cls=mesh, ssid='mesh-ssid', channel=5)
 
     net.plotGraph(max_x=110, max_y=110)
 
@@ -71,6 +71,7 @@ def topology():
     c0.start()
     ap1.start([c0])
     ap2.start([c0])
+    ap3.start([c0])
 
     info("*** Running CLI\n")
     CLI_wifi(net)
