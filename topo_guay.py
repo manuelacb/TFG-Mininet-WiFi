6785d6450f1358816a892f7dd118eed4eb8828df
manuelacb <64306662+manuelacb@users.noreply.github.com>
2020-08-30 18:50:24 +0200

    Add files via upload

--- topo_guay.py
+++ topo_guay.py
@@ -20,27 +20,27 @@ def topology(stp):
 
     info("*** Creating nodes\n")
     sta1 = net.addStation('sta1', mac='00:00:00:00:00:01', IP='10.0.0.1/8',
-                          position='10,80,0', range=50)
+                          position='10,80,0', range=50, f=5)
     sta2 = net.addStation('sta2', mac='00:00:00:00:00:02', IP='10.0.0.2/8',
-                          position='20,80,0', range=50)
+                          position='20,80,0', range=50, f=5)
     sta3 = net.addStation('sta3', mac='00:00:00:00:00:03', IP='10.0.0.3/8',
-                          position='30,80,0', range=50)
+                          position='30,80,0', range=50, f=5)
     sta4 = net.addStation('sta4', mac='00:00:00:00:00:04', ip='10.0.0.4/8',
-                          position='100,101,0')
+                          position='100,101,0', f=5)
     sta5 = net.addStation('sta5', mac='00:00:00:00:00:05', ip='10.0.0.5/8',
-                          position='50,101,0')
+                          position='50,101,0', f=5)
     sta6 = net.addStation('sta6', mac='00:00:00:00:00:06', ip='10.0.0.6/8',
-                          position='50,51,0')
+                          position='50,51,0', f=5)
     sta7 = net.addStation('sta7', mac='00:00:00:00:00:07', ip='10.0.0.7/8',
-                          position='100,51,0')
-    # sta8 = net.addStation('sta8', mac='00:00:00:00:00:08', IP='10.0.0.8/8',
-    #                       position='20,30,0', channel='1')
-    # sta9 = net.addStation('sta9', mac='00:00:00:00:00:09', IP='10.0.0.9/8',
-    #                       position='50,20,0', channel='1')
-    # sta10 = net.addStation('sta10', mac='00:00:00:00:00:10', IP='10.0.0.10/8',
-    #                       position='30,5,0', channel='1')
-    # sta11 = net.addStation('sta11', mac='00:00:00:00:00:11', IP='10.0.0.11/8',
-    #                       position='70,5,0', channel='1')
+                          position='100,51,0', f=5)
+    sta8 = net.addStation('sta8', mac='00:00:00:00:00:08', IP='10.0.0.8/8',
+                          position='40,60,0', f=5)
+    sta9 = net.addStation('sta9', mac='00:00:00:00:00:09', IP='10.0.0.9/8',
+                          position='35,40,0', f=5)
+    sta10 = net.addStation('sta10', mac='00:00:00:00:00:10', IP='10.0.0.10/8',
+                          position='30,30,0', f=5)
+    sta11 = net.addStation('sta11', mac='00:00:00:00:00:11', IP='10.0.0.11/8',
+                          position='40,30,0', f=5)
     if stp:
         ap4 = net.addAccessPoint('ap4', ssid='new-ssid4', mode='g', channel='1',
                                  failMode="standalone", position='100,100,0',
@@ -54,18 +54,12 @@ def topology(stp):
         ap7 = net.addAccessPoint('ap7', ssid='new-ssid7', mode='g', channel='1',
                                  failMode="standalone", position='100,50,0',
                                  stp=True)
-        # ap8 = net.addAccessPoint('ap8', ssid='new-ssid8', mode='g', channel='1',
-        #                          failMode="standalone", position='25,40,0',
-        #                          stp=True)
-        # ap9 = net.addAccessPoint('ap9', ssid='new-ssid9', mode='g', channel='1',
-        #                          failMode="standalone", position='35,40,0',
-        #                          stp=True)
-        # ap10 = net.addAccessPoint('ap10', ssid='new-ssid10', mode='g', channel='1',
-        #                          failMode="standalone", position='30,30,0',
-        #                          stp=True)
-        # ap11 = net.addAccessPoint('ap11', ssid='new-ssid11', mode='g', channel='1',
-        #                          failMode="standalone", position='40,30,0',
-        #                          stp=True)
+        ap9 = net.addAccessPoint('ap9', wlans=4, ssid='ssid9,,,', mode='g',
+                                 failMode="standalone", position='30,40,0',
+                                 stp=True)
+        ap10 = net.addAccessPoint('ap10', wlans=2, ssid='ssid10,', mode='g',
+                                 failMode="standalone", position='30,35,0',
+                                 stp=True)
     else:
         ap4 = net.addAccessPoint('ap4', ssid='new-ssid4', mode='g', channel='1',
                                  failMode="standalone", position='100,100,0')
@@ -75,19 +69,11 @@ def topology(stp):
                                  failMode="standalone", position='50,50,0')
         ap7 = net.addAccessPoint('ap7', ssid='new-ssid7', mode='g', channel='1',
                                  failMode="standalone", position='100,50,0')
-    # ap8 = net.addAccessPoint('ap8', ssid='new-ssid8', channel='1', position='20,30,0')
-    # ap9 = net.addAccessPoint('ap9', ssid='new-ssid9', channel='1', position='50,20,0')
-    # ap10 = net.addAccessPoint('ap10', ssid='new-ssid10', channel='1', position='30,5,0')
-    # ap11 = net.addAccessPoint('ap11', ssid='new-ssid11', channel='1', position='70,5,0')
+        ap9 = net.addAccessPoint('ap9', wlans=4, ssid='ssid9,,,', mode='g',
+                                 failMode="standalone", position='30,40,0')
+        ap10 = net.addAccessPoint('ap10', wlans=2, ssid='ssid10,', mode='g',
+                                 failMode="standalone", position='30,35,0')
 
-    sta8 = net.addStation('sta8', mac='00:00:00:00:00:08', IP='10.0.0.8/8', position='40,60,0')
-    # ap8 = net.addAccessPoint('ap8', wlans=2, ssid='ssid8,', mode='g', failMode="standalone", position='25,40,0')
-    sta9 = net.addStation('sta9', mac='00:00:00:00:00:09', IP='10.0.0.9/8', position='35,40,0')
-    ap9 = net.addAccessPoint('ap9', wlans=4, ssid='ssid9,,,', mode='g', failMode="standalone", position='30,40,0', stp=True)
-    sta10 = net.addStation('sta10', mac='00:00:00:00:00:10', IP='10.0.0.10/8', position='30,30,0')
-    ap10 = net.addAccessPoint('ap10', wlans=2, ssid='ssid10,', mode='g', failMode="standalone", position='30,35,0', stp=True)
-    sta11 = net.addStation('sta11', mac='00:00:00:00:00:11', IP='10.0.0.11/8', position='40,30,0')
-    # ap11 = net.addAccessPoint('ap11', wlans=2, ssid='ssid11,', mode='g', failMode="standalone", position='40,30,0', stp=True)
 
     ap1 = net.addAccessPoint('ap1', wlans=3, ssid='ssid1,,', position='20,100,0', channel='1')
     ap2 = net.addAccessPoint('ap2', wlans=3, ssid='ssid2,,', position='40,100,0', channel='1')
@@ -125,8 +111,6 @@ def topology(stp):
     net.addLink(ap2, ap4)
 
     net.addLink(ap9, ap10)
-    # net.addLink(ap9, ap11)
-    # net.addLink(ap3, ap8)
     net.addLink(ap3, ap9)
 
     net.addLink(ap1, intf='ap1-wlan3', cls=mesh, ssid='mesh-ssid', channel='5')
@@ -146,10 +130,8 @@ def topology(stp):
     ap5.start([c0])
     ap6.start([c0])
     ap7.start([c0])
-    # ap8.start([c0])
     ap9.start([c0])
     ap10.start([c0])
-    # ap11.start([c0])
 
 
     info("*** Running CLI\n")
